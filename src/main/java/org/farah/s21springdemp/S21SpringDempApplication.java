417aeee85216d0c2ae06d65ec96b62ddbb013009
Hamza Farah <dado.org@hotmail.com>
2024-03-29 10:43:38 +0100

    added another endpiont

--- src/main/java/org/farah/s21springdemp/S21SpringDempApplication.java
+++ src/main/java/org/farah/s21springdemp/S21SpringDempApplication.java
@@ -4,9 +4,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
 public class S21SpringDempApplication {
@@ -28,4 +29,17 @@ class HomeController {
         LOGGER.info("fetched data:::::::::::::");
         return "hello world";
     }
+
+    List<Fruits> fruits = List.of(new Fruits("Mongo", "Tropical Fruit"), new Fruits("Ananas", "Summer Fruits"));
+
+    @GetMapping("/fruits")
+    private List<Fruits> getAll() {
+        LOGGER.info("all fruits coming, {}", fruits);
+        return fruits;
+    }
+
+}
+
+record Fruits(String name, String description) {
+
 }
