eb8f76c75147c17890cf6373971eceddc773ee60
Hamza Farah <dado.org@hotmail.com>
2024-03-29 11:57:24 +0100

    added scheduler

--- src/main/java/org/farah/s21springdemp/S21SpringDempApplication.java
+++ src/main/java/org/farah/s21springdemp/S21SpringDempApplication.java
@@ -2,14 +2,21 @@ package org.farah.s21springdemp;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.MediaType;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClient;
 
+import java.awt.*;
 import java.util.List;
 
 @SpringBootApplication
+@EnableScheduling
 public class S21SpringDempApplication {
 
     public static void main(String[] args) {
@@ -38,6 +45,21 @@ class HomeController {
         return fruits;
     }
 
+
+    RestClient restClient = RestClient.create();
+
+    @Scheduled(cron = "* * * ? * *")
+    @GetMapping("/hello")
+    private void clientScheduler() {
+
+        String body = restClient.get()
+                .uri("https://s2i-spring.apps.sandbox-m2.ll9k.p1.openshiftapps.com/fruits")
+                .accept(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .body(String.class);
+        LOGGER.info("coming here...., {}", body);
+
+    }
 }
 
 record Fruits(String name, String description) {
